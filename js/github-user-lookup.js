4af6ae04005092f207c962937a52f5a74f96c247
Chance Neff <crneff84@gmail.com>
2016-10-14 09:43:59 -0700

    update index page

--- js/github-user-lookup.js
+++ js/github-user-lookup.js
@@ -5,9 +5,12 @@ Lookup = function() {
 
 Lookup.prototype.getRepos = function(username){
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    console.log(response.name);
+    $('#error').empty();
+    $('#name').text("User's Name: " + response.name);
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    $('#error').empty();
+    $('#name').empty();
+    $('#error').text("Error: User " + error.responseJSON.message);
   });
 };
 
