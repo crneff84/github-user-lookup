094a15eae8d948cf4255590e0fbaf9fb1f119ad7
Chance Neff <crneff84@gmail.com>
2016-10-14 10:43:31 -0700

    api returns user repo name and description and lists on index html

--- js/github-user-lookup.js
+++ js/github-user-lookup.js
@@ -4,12 +4,15 @@ Lookup = function() {
 };
 
 Lookup.prototype.getRepos = function(username){
-  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + username + '/repos' + '?access_token=' + apiKey).then(function(response){
     $('#error').empty();
-    $('#name').text("User's Name: " + response.name);
+    $('#user-repos').empty();
+    for (i = 0; i < response.length; i++){
+      $('#user-repos').append("<ul>" + "<li> Repository Name: " + response[i].name + "</li>" + "<li> Repository Description: " + response[i].description + "</ul>");
+    }
   }).fail(function(error){
     $('#error').empty();
-    $('#name').empty();
+    $('#user-repos').empty();
     $('#error').text("Error: User " + error.responseJSON.message);
   });
 };
